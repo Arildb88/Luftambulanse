a62efd59f05b646b05f6f76ae72e0c5caefd6211
Arild <arild_bjornetro@hotmail.com>
2025-09-23 11:31:39 +0200

    Endret for å teste

--- obj/Debug/net9.0/Gruppe4NLA.AssemblyInfo.cs
+++ obj/Debug/net9.0/Gruppe4NLA.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gruppe4NLA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ebe7387bb41bccb303539053f707b6118678638")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5dbde5270b88e1f991456b8d3a5032875861700")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gruppe4NLA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gruppe4NLA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
