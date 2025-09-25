493f3fa5d63cabc344c46a39b6432aff884003c2
Elocked <einar.alme@protonmail.com>
2025-09-25 19:10:04 +0200

    New page with coordinates submission. This has map integrated. Illegal inputs will not be allowed. Map is pop-up and gives coordinates if saved. Can integrate other variables for a full report. Data resets after quitting. Visible in top menu.

---
+++ Controllers/CoordinatesController.cs
@@ -0,0 +1,48 @@
+﻿
+// Einar
+
+using Gruppe4NLA.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gruppe4NLA.Controllers
+{
+    public class CoordinatesController : Controller
+    {
+        // For demo, keep list in memory
+        private static List<CoordinatesViewModel> _submittedCoordinates = new List<CoordinatesViewModel>();
+
+        [HttpGet]
+        public IActionResult CoordinatesTest()
+        {
+            var model = new CoordinatesViewModelWrapper
+            {
+                SubmittedCoordinates = _submittedCoordinates
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult CoordinatesTest(CoordinatesViewModelWrapper model)
+        {
+            if (!ModelState.IsValid)
+            {
+                // Return view with validation errors
+                model.SubmittedCoordinates = _submittedCoordinates;
+                return View(model);
+            }
+
+            // Save valid coordinate
+            _submittedCoordinates.Add(new CoordinatesViewModel
+            {
+                Latitude = model.NewCoordinate.Latitude,
+                Longitude = model.NewCoordinate.Longitude
+            });
+
+            model.NewCoordinate = new CoordinatesViewModel(); // Reset input
+            model.SubmittedCoordinates = _submittedCoordinates;
+
+            ViewBag.Message = "Coordinate submitted successfully!";
+            return View(model);
+        }
+    }
+}
