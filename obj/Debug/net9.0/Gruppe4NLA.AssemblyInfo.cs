5f51256c78a1ab7b80ce582a45fbc1ac201a2012
Encloud <einar.alme@protonmail.com>
2025-10-02 16:38:55 +0200

    Stash and pull before fixing.

--- obj/Debug/net9.0/Gruppe4NLA.AssemblyInfo.cs
+++ obj/Debug/net9.0/Gruppe4NLA.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gruppe4NLA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+530b07d18b3049ea12c8c186d47d98a5cf551678")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35de140f6bce5057d62034c08fb6f99f40201eaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gruppe4NLA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gruppe4NLA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
