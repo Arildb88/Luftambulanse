0cd934e145f5e01ce1710ae951363f9cc10e4625
einar <einar.alme@protonmail.com>
2025-10-20 14:33:52 +0200

    User managment test

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,5 +1,9 @@
 using Gruppe4NLA.DataContext;
 using Microsoft.EntityFrameworkCore;
+using Gruppe4NLA.Data.Areas.Identity.Data;
+using Gruppe4NLA.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +11,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseMySql(builder.Configuration.GetConnectionString("OurDbConnection"), 
     new MySqlServerVersion(new Version(11, 8, 3))));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationContext>();
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
 
 var app = builder.Build();
 
@@ -18,16 +28,28 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
+
+
+
 app.UseHttpsRedirection();
 app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
 app.MapStaticAssets();
 
 // Needed to load local leaflet map
-app.UseStaticFiles(); 
+app.UseStaticFiles();
 
+app.MapRazorPages();
+
+app.MapControllerRoute(
+    name: "Areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+);
 
 app.MapControllerRoute(
     name: "default",
