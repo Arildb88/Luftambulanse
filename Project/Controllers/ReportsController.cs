019fb71d3f04b024d6d00e885e749b296f21fb4b
Encloud <einar.alme@protonmail.com>
2025-11-02 00:03:00 +0100

    Added polyline button and marker button. One removes the other. Needs improvement.

--- Project/Controllers/ReportsController.cs
+++ Project/Controllers/ReportsController.cs
@@ -73,6 +73,7 @@ namespace Gruppe4NLA.Controllers
                 UserId = model.NewReport.UserId,
                 Latitude = model.NewReport.Latitude,
                 Longitude = model.NewReport.Longitude,
+                GeoJson = model.NewReport.GeoJson,
                 SenderName = model.NewReport.SenderName,
                 Type = model.NewReport.Type,
                 OtherDangerType = model.NewReport.OtherDangerType,
@@ -80,6 +81,7 @@ namespace Gruppe4NLA.Controllers
                 HeightInnMeters = model.NewReport.HeightInnMeters,
                 AreLighted = model.NewReport.AreLighted,
                 DateSent = DateTime.Now
+                
             };
 
             _context.Reports.Add(newReport);
@@ -119,6 +121,7 @@ namespace Gruppe4NLA.Controllers
                 UserId = model.NewReport.UserId,
                 Latitude = model.NewReport.Latitude,
                 Longitude = model.NewReport.Longitude,
+                GeoJson = model.NewReport.GeoJson,
                 SenderName = model.NewReport.SenderName,
                 Type = model.NewReport.Type,
                 OtherDangerType = model.NewReport.OtherDangerType,
